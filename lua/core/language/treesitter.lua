5ead541903a7c743bf7685736d12ee89c8c6b5df
Ganesha Danu Enastika <ganeshadanu1@gmail.com>
2023-07-05 23:19:50 +0700

    treesitter highlighting

--- lua/core/language/treesitter.lua
+++ lua/core/language/treesitter.lua
@@ -1,3 +1,12 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = { "lua", "jsonc" },
 })
+
+require("nvim-treesitter.configs").setup({
+	sync_install = false,
+
+	highlight = {
+		enable = true,
+		additional_vim_regex_highlighting = false,
+	},
+})
